6c555092fb79bbc509c9753ab42c863071b836c6
cristina1084 <cristina.johns1084@gmail.com>
2019-08-02 14:22:46 +0530

    library

--- html/js/library.js
+++ html/js/library.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){             //jQuery
     $("#load").hide();
-    $("#tp1,#cat li").click(function(){
+    $("#card2").hide();
+    $("#tp1,#tp2,#cat li").click(function(){
         var cat = $(this).text();
         console.log(cat);
-        
+        if(cat=="LOGIN"){
+            $("#card1").show();
+            $("#card2").hide();
+        }
+        else if(cat=="REGISTER"){
+            $("#card2").show();
+            $("#card1").hide();
+        } 
+
         $.ajax({                        //AJAX format
             type:"GET",                 //type mentions the method to be used 
             url :"librarydata.json",         //can provide API link
@@ -11,13 +20,13 @@ $(document).ready(function(){             //jQuery
                 $("#load").show();
             },
             success : function(data){
-                $("#load").hide();
+                $("#load,#card1,#card2").hide();
                 /* console.log(data); */
                 var card="<div class='card-deck'>";
                 for (var j in data){
-                    if (cat == data[j].genre || cat == 'Books')
+                    if (cat == data[j].genre || cat == 'View All')
                     {
-                        card+="<div class='col-4 col-sm-4 col-md-4'> <div class='card'> <img class='card-img-top' src=" + data[j].urlToImage + ">";
+                        card+="<div class='col-4 col-sm-4 col-md-4'> <div class='card'> <img class='card-img-top' src=" + data[j].urlToImage + " height='400px' width='150px'>";
                         card+="<div class='card-body'> <h5 class='card-title'>"+ data[j].bookTitle +"</h5>";
                         card+="<h6 class='card-subtitle mb-2 text-muted'>" + data[j].author +"</h6> ";
                         card+="<p class='card-text text-justify'>"+ data[j].description + "</p> </div>";
